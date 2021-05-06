facf0c278e015e2a94392ae4ec3f3612b34dc4d9
Michael Hayman <mdghayman@gmail.com>
2021-05-06 20:08:43 +0100

    - Made New Game interactive, added columns, and changed it to Level 1. - Added additional New Game layer, with details of Level 1. - Improved game logic, for atmosphere and preparing for government. - Integrated temperature, CO2, sea level, and year with 2 levels of change.

--- app.py
+++ app.py
@@ -1,96 +1,100 @@
 from flask import Flask, render_template, redirect, session, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from flask_login import (LoginManager, UserMixin, login_user, login_required,
-        logout_user, current_user)
-from werkzeug.security import generate_password_hash, check_password_hash
+from model import atmosphere, government
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/new_game')
+
+@app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
     return render_template('new_game.html')
+
+
+@app.route('/level_1', methods=['GET', 'POST'])
+def level_1():
+
+    year = atmosphere.year
+    co2 = atmosphere.co2
+    temp = atmosphere.temp
+    sea_level = atmosphere.sea_level
+
+    if request.method == 'POST':
+
+        if request.form['emissions'] == 'decrease':
+            atmosphere.year += 1
+            atmosphere.co2 += 1.9
+            atmosphere.temp += 0.65
+            atmosphere.sea_level += 1.5
+            return render_template('level_1.html',
+                year=atmosphere.year,
+                co2='{:.1f}'.format(atmosphere.co2),
+                temp='{:.1f}'.format(atmosphere.temp),
+                sea_level='{:.1f}'.format(atmosphere.sea_level))
+
+        if request.form['emissions'] == 'increase':
+            atmosphere.year += 1
+            atmosphere.co2 += 2.1
+            atmosphere.temp += 1.06
+            atmosphere.sea_level += 1.9
+            return render_template('level_1.html',
+                year=atmosphere.year,
+                co2='{:.1f}'.format(atmosphere.co2),
+                temp='{:.1f}'.format(atmosphere.temp),
+                sea_level='{:.1f}'.format(atmosphere.sea_level))
+
+    elif request.method == 'GET':
+        return render_template('level_1.html',
+            year=atmosphere.year,
+            co2='{:.1f}'.format(atmosphere.co2),
+            temp='{:.1f}'.format(atmosphere.temp),
+            sea_level='{:.1f}'.format(atmosphere.sea_level))
+
 
 @app.route('/how_to_play')
 def how_to_play():
     return render_template('how_to_play.html')
 
+
 @app.route('/about_climagotchi')
 def about_climagotchi():
     return render_template('about_climagotchi.html')
+
 
 @app.route('/about_climate_change')
 def about_climate_change():
     return render_template('about_climate_change.html')
 
+
 @app.route('/climate_change_evidence')
 def climate_change_evidence():
     return render_template('climate_change_evidence.html')
+
 
 @app.route('/climate_change_solutions')
 def climate_change_solutions():
     return render_template('climate_change_solutions.html')
 
+
 @app.route('/climate_finance')
 def climate_finance():
     return render_template('climate_finance.html')
+
 
 @app.route('/climate_finance_and_covid')
 def climate_finance_and_covid():
     return render_template('climate_finance_and_covid.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    response = None
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('model'))
-        response = 'Invalid username or password'
-    return render_template('login.html', form=form, response=response)
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    response = None
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data,
-            method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data,
-            password=hashed_password)
-        if User.query.filter_by(username=new_user.username).first():
-            response = 'Please enter a unique username.'
-        elif User.query.filter_by(email=new_user.email).first():
-            response = 'Please enter a unique email address.'
-        else:
-            db.session.add(new_user)
-            db.session.commit()
-            response = 'New user has been created!'
-    return render_template('signup.html', form=form, response=response)
 
 if __name__ == '__main__':
     app.run()
---
+++ model.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+
+class Atmosphere:
+    def __init__(self, year, co2, temp, sea_level):
+        self.year = year
+        self.co2 = co2 #ppm
+        self.temp = temp #degC
+        self.sea_level = sea_level #mm
+
+class Government:
+    def __init__(self, gdp, health, happiness, tech):
+        self.gdp = gdp
+        self.health = health
+        self.happiness = happiness
+        self.tech = tech
+
+atmosphere = Atmosphere(year=1850, co2=300, temp=20, sea_level=0)
+government = Government(gdp=500, health=0.7, happiness=0.7, tech=0.7)
